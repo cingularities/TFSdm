ac64f777b300ff8ebca64470f479372d9b7e60a9
CLNorton <67613055+cyngularities@users.noreply.github.com>
2020-10-01 16:05:50 -0700

    Update test_data.R

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -1,8 +1,4 @@
-library(testthat)
-library(TFSDataMonitoring)
-
-se_file <- system.file("insta/extdata", "se.csv")
-se <- read.csv(se_file)
+context("Data loading and column count")
 
 
 test_that("Locating data file in package",
@@ -12,12 +8,37 @@ test_that("Locating data file in package",
   }
 )
 
+#> Test passed
+
+test_that(
+  "Checking column result expectations",
+  {
+    se_file<- system.file("insta/extdata", "se.csv")
+    se<- readr::read_csv(se_file)
+    sma_file <- system.file("insta/extdata", "sma.csv")
+    sma<- readr::read_csv(sma_file)
+    sp_file <- system.file("insta/extdata", "sp.csv")
+    sp<- readr::read_csv(sp_file)
+    status_file <- system.file("insta/extdata", "status.csv")
+    status<- readr::read_csv(status_file)
+    clients_file <- system.file("insta/extdata", "clients.csv")
+    clients<- readr::read_csv(clients_file)
+
+    expect_equal(ncol(dm_down(se, sma, sp, status, clients)), 10)
+  }
+)
 
 #> Test passed
 
 test_that(
   "Checking column result expectations",
   {
+    se<- system.file("insta/extdata", "se.csv")
+    sma <- system.file("insta/extdata", "sma.csv")
+    sp<- system.file("insta/extdata", "sp.csv")
+    status <- system.file("insta/extdata", "status.csv")
+    clients <- system.file("insta/extdata", "clients.csv")
+
     expect_equal(ncol(dm_down(se, sma, sp, status, clients)), 10)
   }
 )
