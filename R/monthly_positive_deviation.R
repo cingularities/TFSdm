a80250fee9474030eebaec45cfa3b291e949fb07
CLNorton <67613055+cingularities@users.noreply.github.com>
2021-04-12 14:23:00 -0700

    Update monthly_positive_deviation.R

--- R/monthly_positive_deviation.R
+++ R/monthly_positive_deviation.R
@@ -9,18 +9,31 @@
 #' monthly_positive_deviation(sma_performance,se_performance,estimates,status)
 
 
-monthly_positive_deviation <- function(sma_performance,se_performance,estimates,status) {
-  se_filter <- left_join(se_performance %>% select(PORTAL.NAME,MONTH),
-          estimates %>% select(PORTAL.NAME,Email,
-          Installation.Date,JOB.NAME,MonitorCode,AUG,PvEstMonthlyProd), by = "PORTAL.NAME")
-  sma_filter <- left_join(sma_performanc %>% select(PORTAL.NAME,MONTH),
-          estimates %>% select(PORTAL.NAME,Email,
-          Installation.Date,JOB.NAME,MonitorCode,AUG,PvEstMonthlyProd), by = "PORTAL.NAME")
-  se_sma_bind <- rbind(se_filter, sma_filter)
+monthly_positive_deviation <- function(sma_performance_raw,se_performance_raw,estimates,status) {
+  se_performance <- se_performance_raw %>% select(-c(1,3)) %>%
+    slice(-c(1:8)) %>%
+    rename_(PORTAL.NAME = 1, MONTH = 2) %>%
+    mutate(PORTAL.SERVER = "SE")  %>%
+    select(PORTAL.NAME, MONTH, PORTAL.SERVER) %>%
+    left_join(estimates %>% select(PORTAL.NAME,Email,Installation.Date,JOB.NAME,MonitorCode,MAR,PvEstMonthlyProd), by = "PORTAL.NAME") %>%
+    left_join(status %>% select(PORTAL.NAME, STATUS, CATEGORY, DATE, YEAR, NOTES), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
+    distinct(PORTAL.NAME, .keep_all= TRUE)
+  
+  sma_performance <- sma_performance_raw %>% select(-c(2,3,4,6,7,8)) %>%
+    rename_(PORTAL.NAME = 1, MONTH = 2, YEAR = 3) %>%
+    mutate(PORTAL.SERVER = "SMA") %>%
+    select(PORTAL.NAME, MONTH, PORTAL.SERVER) %>%
+    left_join(estimates %>% select(PORTAL.NAME,Email,Installation.Date,JOB.NAME,MonitorCode,MAR,PvEstMonthlyProd), by = "PORTAL.NAME") %>%
+    left_join(status %>% select(PORTAL.NAME, STATUS, CATEGORY, DATE, YEAR, NOTES), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
+    distinct(PORTAL.NAME, .keep_all= TRUE)
+  
+  se_sma_bind <- rbind(se_performance, sma_performance)
+  
   se_sma_deviation <- se_sma_bind %>%
-    transform(PERFORMANCE = as.numeric(MONTH)/as.numeric(AUG))%>%
+    transform(PERFORMANCE = as.numeric(MONTH)/as.numeric(MAR))%>%
     left_join(status %>% select(PORTAL.NAME,STATUS, CATEGORY), by = "PORTAL.NAME")
-  positive <- filter(se_sma_deviation, PERFORMANCE >  0.85)
-  distinct(positive, PORTAL.NAME, .keep_all= TRUE)
+  
+  positive <- filter(se_sma_deviation, PERFORMANCE > 0.85)
+  
   return(positive)
 }
