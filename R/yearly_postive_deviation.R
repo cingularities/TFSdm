0c61427ac444f1202b04d400484fe47c4d64bd68
CLNorton <67613055+cingularities@users.noreply.github.com>
2021-04-12 14:22:05 -0700

    Update yearly_postive_deviation.R

--- R/yearly_postive_deviation.R
+++ R/yearly_postive_deviation.R
@@ -11,17 +11,28 @@
 
 
 yearly_positive_deviation <- function(sma_performance,se_performance,estimates,status) {
-  se_filter <- left_join(se_performance %>% select(PORTAL.NAME,YEAR),
-            estimates %>% select(PORTAL.NAME,Address,City,State,Postal,Email,
-            Installation.Date,JOB.NAME,MonitorCode,PvEstYearlyProd), by = "PORTAL.NAME")
-  sma_filter <- left_join(sma_performance %>% select(PORTAL.NAME,YEAR),
-            estimates %>% select(PORTAL.NAME,Address,City,State,Postal,Email,
-            Installation.Date,JOB.NAME,MonitorCode,PvEstYearlyProd), by = "PORTAL.NAME")
-  se_sma_bind <- rbind(se_filter, sma_filter)
+  se_performance <- se_performance_raw %>% select(-c(1,3)) %>%
+    slice(-c(1:8)) %>%
+    rename_(PORTAL.NAME = 1, YEAR = 2) %>%
+    mutate(PORTAL.SERVER = "SE")  %>%
+    select(PORTAL.NAME, YEAR, PORTAL.SERVER) %>%
+    left_join(estimates %>% select(PORTAL.NAME,Address,City,State,Postal,Email,Installation.Date,JOB.NAME,MonitorCode,PvEstYearlyProd), by = "PORTAL.NAME")
+  
+  sma_performance <- sma_performance_raw %>% select(-c(2,3,4,6,7,8)) %>%
+    rename_(PORTAL.NAME = 1, MONTH = 2, YEAR = 3) %>%
+    mutate(PORTAL.SERVER = "SMA") %>%
+    select(PORTAL.NAME, YEAR, PORTAL.SERVER) %>%
+    left_join(estimates %>% select(PORTAL.NAME,Address,City,State,Postal,Email,Installation.Date,JOB.NAME,MonitorCode,PvEstYearlyProd), by = "PORTAL.NAME")
+  
+  se_sma_bind <- rbind(se_performance, sma_performance)
+  
   se_sma_deviation <- se_sma_bind %>%
     transform(PERFORMANCE = (as.numeric(YEAR)/PvEstYearlyProd)*100)%>%
+    left_join(status %>% select(PORTAL.NAME,STATUS, CATEGORY), by = "PORTAL.NAME")
+  
   positive <- filter(se_sma_deviation, PERFORMANCE > 95.0)
+  
   distinct(positive, PORTAL.NAME, .keep_all= TRUE)
+  
   return(positive)
 }
-
