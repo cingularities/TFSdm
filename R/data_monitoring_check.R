f789fda42d051e7dd9742be6ebb8fe685374a374
CLNorton <67613055+cingularities@users.noreply.github.com>
2021-05-06 11:58:48 -0700

    Update data_monitoring_check.R

--- R/data_monitoring_check.R
+++ R/data_monitoring_check.R
@@ -15,8 +15,8 @@ data_monitoring_check<- function(se_raw, sma_raw, sp_raw, lg_raw, status, client
       se <- se_raw %>% select(-c(1,3)) %>% #removes unwanted columns
         slice(-c(1:8)) %>% #removes unwanted rows
         rename(PORTAL.NAME = 1, Severity = 2, kWh =3) %>% #renames columns
-        mutate(PORTAL.SERVER = "SE")  %>% #adds PORTAL.SERVER column
-        filter(Severity > "0" | kWh == "0") %>% #filters sites that have high and low severity, no data and 0 production
+        mutate(PORTAL.SERVER = "SE") %>% #adds PORTAL.SERVER column
+        filter(Severity > "0" | kWh == "0.0") %>% #filters sites that have high and low severity, no data and 0 production
         select(PORTAL.NAME, PORTAL.SERVER) %>% #selects columns wanted
         left_join(status %>% select(PORTAL.NAME, STATUS, CATEGORY, DATE, YEAR, NOTES), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
         left_join(clients, by = "PORTAL.NAME") #joins filtered & joined recent down dm list with the portal key to gather FACILITY.NAME and JOB.NAME
@@ -24,9 +24,9 @@ data_monitoring_check<- function(se_raw, sma_raw, sp_raw, lg_raw, status, client
       sma <- sma_raw %>% select(-c(2,3,5,6,7,8,9)) %>% #removes unwanted columns
         rename(PORTAL.NAME = 1, YESTERDAY = 2) %>% #renames column
         mutate(PORTAL.SERVER = "SMA")%>% #adds PORTAL.SERVER column
-        filter(YESTERDAY == "No data" | YESTERDAY == "0") %>% #filters sites that have no data and 0 production
+        filter(YESTERDAY== "No data" | YESTERDAY == "0.00") %>% #filters sites that have no data and 0 production
         select(PORTAL.NAME, PORTAL.SERVER)%>% #selects columns wanted
-        left_join(status %>% select(PORTAL.NAME, STATUS, CATEGORY, DATE, YEAR, NOTES), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
+        left_join(status%>% select(PORTAL.NAME, STATUS, CATEGORY, DATE, YEAR, NOTES), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
         left_join(clients, by = "PORTAL.NAME")#joins filtered & joined recent down dm list with the portal key to gather FACILITY.NAME and JOB.NAME
       
       sp <- sp_raw %>% select(-c(2,4,8,9,10)) %>% #removes unwanted columns 
