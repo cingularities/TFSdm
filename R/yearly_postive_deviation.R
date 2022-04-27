a17e19be35c4add445fb73b4bc59f1d2546d8cb4
CLNorton <67613055+cingularities@users.noreply.github.com>
2022-04-27 11:17:25 -0700

    Update yearly_postive_deviation.R

--- R/yearly_postive_deviation.R
+++ R/yearly_postive_deviation.R
@@ -8,9 +8,7 @@
 #' @examples
 #' yearly_positive_deviation(sma_performance,se_performance,estimates,status)
 
-
-
-yearly_positive_deviation <- function(sma_performance_yearly,se_performance_yearly,estimates_raw,status) {
+yearly_positive_deviation <- function(sma_performance_yearly,se_performance_yearly,estimates_raw,status_raw) {
   
   estimates <- estimates_raw %>% group_by(JobName) %>% slice(which.max(YearlyEstimate)) %>% select(c(PORTAL.NAME,PORTAL.SERVER,JobAddress, City, State, Postal, Email, InstallationDate, JobName, MonitorCode, YearlyEstimate))
   
@@ -19,20 +17,20 @@ yearly_positive_deviation <- function(sma_performance_yearly,se_performance_year
     rename_(PORTAL.NAME = 1, YEAR = 2) %>% #renames column
     select(PORTAL.NAME, YEAR) %>%  #selects columns wanted
     mutate(PORTAL.SERVER = "SE")%>%  #adds PORTAL.SERVER column
-    left_join(estimates %>% select(PORTAL.NAME,PORTAL.SERVER,JobAddress, City, State, Postal, Email, InstallationDate, JobName, MonitorCode, YearlyEstimate), by = c("PORTAL.NAME","PORTAL.SERVER")) #joins with performance residence
+    left_join(estimates %>% select(PORTAL.NAME,PORTAL.SERVER,JobAddress, City, State, Postal, Email, InstallationDate, JobName, MonitorCode, YearlyEstimate), by = "PORTAL.NAME") #joins with performance residence
   
   sma_performance <- sma_performance_yearly %>% select(-c(3,4,6,7,8)) %>% #Selects column
     rename_(PORTAL.NAME = 1, SYSTEM.SIZE = 2, MONTH = 3, RAW = 4) %>% #renames column
     transform(YEAR = (as.numeric(RAW)*as.numeric(SYSTEM.SIZE)))%>% #Sunnyportal exports yearly production kWh/kWp, reference file is in kWh. This transforms YEAR data to kWh to match reference. 
     select(PORTAL.NAME, YEAR) %>% #selects columns wanted
     mutate(PORTAL.SERVER = "SMA")%>%  #adds PORTAL.SERVER column
-    left_join(estimates %>% select(PORTAL.NAME,PORTAL.SERVER,JobAddress, City, State, Postal, Email, InstallationDate, JobName, MonitorCode, YearlyEstimate), by = c("PORTAL.NAME","PORTAL.SERVER")) #joins with performance residence
+    left_join(estimates %>% select(PORTAL.NAME,PORTAL.SERVER,JobAddress, City, State, Postal, Email, InstallationDate, JobName, MonitorCode, YearlyEstimate), by = "PORTAL.NAME") #joins with performance residence
   
   se_sma_bind <- rbind(se_performance, sma_performance) #binds columns two portals
   
   se_sma_deviation <- se_sma_bind %>%
     transform(PERFORMANCE = (as.numeric(YEAR)/YearlyEstimate)*100)%>% #calculating performance based on TFS reference numbers. actual production/estimated production
-    left_join(status %>% select(PORTAL.NAME,STATUS, CATEGORY), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
+    left_join(status_raw %>% select(PORTAL.NAME,STATUS, CATEGORY), by = "PORTAL.NAME")%>% #joins recent down dm list with current portal status
     filter(PERFORMANCE > 99)%>% #filters sites that are performing above 95%
     distinct(PORTAL.NAME, .keep_all= TRUE)
   
